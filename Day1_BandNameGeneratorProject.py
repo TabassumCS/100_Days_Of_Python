0f69981221c9487bc07deaa8f1483a14e70863f4
Tabassum <tabassumabedi2024@gmail.com>
2025-04-15 19:35:02 +0100

    Day 1 Project completed

---
+++ Day1_BandNameGeneratorProject.py
@@ -0,0 +1,6 @@
+# DAY 1 - Band Name Generator Project
+# This is a simple band name generator program.
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
